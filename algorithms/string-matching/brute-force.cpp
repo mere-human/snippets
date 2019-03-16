8923114609819939066bfd5f5ec5ef571bca4f89
mere-human <9664141+mere-human@users.noreply.github.com>
2019-03-16 21:18:28 +0200

    Reformat sources

--- algorithms/string-matching/brute-force.cpp
+++ algorithms/string-matching/brute-force.cpp
@@ -1,39 +1,39 @@
-#include <string>
 #include <iostream>
+#include <string>
 
 int search(const std::string &pat, const std::string &txt)
 {
-    int m = pat.length();
-    int n = txt.length();
-    int i = 0; // points to the end of sequence of already-matched characters
-    int j = 0;
-    for (; i < n && j < m; ++i)
+  int m = pat.length();
+  int n = txt.length();
+  int i = 0; // points to the end of sequence of already-matched characters
+  int j = 0;
+  for (; i < n && j < m; ++i)
+  {
+    if (txt[i] == pat[j])
     {
-        if (txt[i] == pat[j])
-        {
-            ++j;
-        }
-        else
-        {
-            // mismatch - backup
-            i -= j;
-            j = 0;
-        }
+      ++j;
     }
+    else
+    {
+      // mismatch - backup
+      i -= j;
+      j = 0;
+    }
+  }
 
-    if (j == m)
-        return i - m; // found
-    return -1;        // not found
+  if (j == m)
+    return i - m; // found
+  return -1;      // not found
 }
 
 int main()
 {
-    std::string txt = "abacadabrac";
-    std::string pat = "abra";
-    auto pos = search(pat, txt);
-    if (pos >= 0)
-        std::cout << "Found at " << pos << "\n";
-    else
-        std::cout << "Not found\n";
-    return 0;
+  std::string txt = "abacadabrac";
+  std::string pat = "abra";
+  auto pos = search(pat, txt);
+  if (pos >= 0)
+    std::cout << "Found at " << pos << "\n";
+  else
+    std::cout << "Not found\n";
+  return 0;
 }
--- algorithms/string-matching/kmp.cpp
+++ algorithms/string-matching/kmp.cpp
@@ -2,29 +2,35 @@
 #include <string>
 #include <vector>
 
-int search(const std::string &pat, const std::string &txt) {
+int search(const std::string &pat, const std::string &txt)
+{
   const size_t m = pat.length();
   const size_t r = 128;
   // build DFA from pattern
   // array int[R][M]
   std::vector<std::vector<int>> dfa(r, std::vector<int>(m));
   dfa[pat[0]][0] = 1;
-  for (int x = 0, j = 1; j < m; ++j) {
+  for (int x = 0, j = 1; j < m; ++j)
+  {
     // compute dfa[][j]
-    for (int c = 0; c < r; ++c) dfa[c][j] = dfa[c][x];  // copy mismatch cases
-    dfa[pat[j]][j] = j + 1;                             // set match case
-    x = dfa[pat[j]][x];                                 // update restart state
+    for (int c = 0; c < r; ++c)
+      dfa[c][j] = dfa[c][x]; // copy mismatch cases
+    dfa[pat[j]][j] = j + 1;  // set match case
+    x = dfa[pat[j]][x];      // update restart state
   }
   // simulate operation of DFA on txt
   const int n = txt.length();
   int i = 0;
   int j = 0;
-  for (; i < n && j < m; ++i) j = dfa[txt[i]][j];
-  if (j == m) return i - m;  // found (hit end of pattern)
-  return -1;                 // not found (hit end of text)
+  for (; i < n && j < m; ++i)
+    j = dfa[txt[i]][j];
+  if (j == m)
+    return i - m; // found (hit end of pattern)
+  return -1;      // not found (hit end of text)
 }
 
-int main() {
+int main()
+{
   std::string txt = "abacadabrac";
   std::string pat = "abra";
   auto pos = search(pat, txt);
