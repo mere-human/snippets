70142a2b4dae83b6d4f149c51475b6f800b94392
mere-human <9664141+mere-human@users.noreply.github.com>
2022-08-14 21:46:42 +0300

    Add generating HTML result

--- twitter/user_tweets.py
+++ twitter/user_tweets.py
@@ -3,6 +3,8 @@ Script that obtains tweets of a specific account.
 Examples:
 https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/User-Lookup/get_users_with_user_context.py
 https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/User-Tweet-Timeline/user_tweets.py
+API playground:
+https://oauth-playground.glitch.me/
 '''
 
 import requests
@@ -33,7 +35,8 @@ def get_user_id(username):
     url = f"https://api.twitter.com/2/users/by"
     params = {'usernames': username}
     response = requests.request("GET", url, params=params, auth=bearer_oauth)
-    assert (response.status_code == 200)
+    if response.status_code != 200:
+        response.raise_for_status()
     jresp = response.json()
     return jresp['data'][0]['id']
 
@@ -59,7 +62,51 @@ def get_tweets(uid, max_results=None, pagination_token=None):
 
 
 def parse_tweets(tweets_json):
-    print(json.dumps(tweets_json, indent=4, sort_keys=True))
+    # print(json.dumps(tweets_json, indent=4, sort_keys=True))
+    html_prefix = '''
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    '''
+    html_suffix ='''
+</body>
+</html>
+    '''
+    html_item ='''
+<h2>{date}</h2>
+<p>{text}</p>
+{extra}
+<hr/>
+    '''
+    html_img = '<img src="{url}">'
+    html_result = []
+    html_result.append(html_prefix)
+    media_list = tweets_json['includes']['media']
+    media_photos = {}
+    for media in media_list:
+        if media['type'] == 'photo':
+            media_photos[media['media_key']] = media['url']
+    for tweet in tweets_json['data']:
+        # TODO: skip w/o attachments
+        # TODO: substitute t.co link?
+        attachement_list = tweet.get('attachments')
+        if attachement_list:
+            attachement_list = attachement_list['media_keys']
+        else:
+            attachement_list = []
+        created = tweet['created_at']
+        text = tweet['text']
+        html_extra = []
+        for attachement in attachement_list:
+            img_url = media_photos[attachement]
+            html_extra.append(html_img.format(url=img_url))
+        html_tweet = html_item.format(date=created, text=text, extra=''.join(html_extra))
+        html_result.append(html_tweet)
+    html_result.append(html_suffix)
+    with open('result.html', 'w') as f:
+        f.write(''.join(html_result))
 
 
 def main():
